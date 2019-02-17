279a30a33c3cd5b1aaed304117fc3ba8b5662ffc
iamayush18 <i.am.ayush@outlook.com>
2019-02-17 16:19:03 +0530

    New Commit

--- js/script.js
+++ js/script.js
@@ -1,41 +1,54 @@
-function getname() {
-  const e = document.getElementById('inp');
-  var name = e.value;
-  return name;
-}
-const app = document.getElementById('root');
+var click = document.getElementById("srch");
+click.onclick = function (){
+  var name = document.getElementById('inp').value;
+  
+  const b = document.getElementById('page');
+  b.style.display = "none";
+  const l = document.getElementById('load');
+  l.style.display = "block";
 
-const container = document.createElement('div');
-container.setAttribute('class', 'container');
-app.appendChild(container);
+  const app = document.getElementById('root');
+  const container = document.createElement('div');
+  container.setAttribute('class', 'container');
+  app.appendChild(container);
 
-var request = new XMLHttpRequest();
-var name = getname();
-request.open('GET', 'https://api.social-searcher.com/v2/search?q='+name+'&limit=100&key=489b251c8c61b6396996e79670211b62', true);
-request.onload = function () {
-  var data = JSON.parse(this.response);
-  if (this.status >= 200 && this.status < 400) {
-    data.posts.forEach(post => {
-      const card = document.createElement('div');
-      card.setAttribute('class', 'card');
+  var request = new XMLHttpRequest();
+  request.open('GET', 'https://api.social-searcher.com/v2/search?q='+name+'&limit=100&key=489b251c8c61b6396996e79670211b62', true);
+  request.onload = function () {
+    var data = JSON.parse(this.response);
+    l.style.display = "none";
 
-      const h1 = document.createElement('h1');
-      h1.textContent = post.network;
+    if (this.status >= 200 && this.status < 400) {
+      data.posts.forEach(post => {
+        const card = document.createElement('div');
+        card.setAttribute('class', 'card');
 
-      const p = document.createElement('p');
-      p.textContent = "Post : " + post.text + "Sentiment : " + post.sentiment + "Link : " + post.url;
+        const h1 = document.createElement('h1');
+        h1.setAttribute('class', 'head');
+        h1.textContent = post.network;
 
-      container.appendChild(card);
-      card.appendChild(h1);
-      card.appendChild(p);
-    });
-  } else {
-    const errorMessage = document.createElement('p');
-    //errorMessage.style.color = red;
-    //errorMessage.style.fontSize = 30;
-    errorMessage.textContent = "Error 400 !";
-    app.appendChild(errorMessage);
+        const p = document.createElement('p');
+        p.setAttribute('class', 'para');
+        p.textContent = "Post : " + post.text + "Sentiment : " + post.sentiment + "Link : " + post.url;
+
+        const img = document.createElement('img');
+        img.setAttribute('class', 'image');
+        var imgsrc = post.user.image;
+        img.setAttribute('src', imgsrc);
+
+        container.appendChild(card);
+        card.appendChild(h1);
+        card.appendChild(p);
+        card.appendChild(img);
+      });
+    } else {
+      const errorMessage = document.createElement('p');
+      //errorMessage.style.color = red;
+      //errorMessage.style.fontSize = 30;
+      errorMessage.textContent = "Error 400 !";
+      app.appendChild(errorMessage);
+    }
   }
-}
 
-request.send();
+  request.send();
+}
