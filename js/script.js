e0f09c820ed1a974b23cb59738801394373e0d5d
iamayush18 <i.am.ayush@outlook.com>
2019-02-18 11:44:23 +0530

    Added Sentiment based colored cards & images

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var click = document.getElementById("srch");
+var click = document.getElementById('srch');
 click.onclick = function (){
   var name = document.getElementById('inp').value;
   
@@ -6,6 +6,8 @@ click.onclick = function (){
   b.style.display = "none";
   const l = document.getElementById('load');
   l.style.display = "block";
+  const lt = document.getElementById('loadtext');
+  lt.style.display = "block";
 
   const app = document.getElementById('root');
   const container = document.createElement('div');
@@ -17,6 +19,7 @@ click.onclick = function (){
   request.onload = function () {
     var data = JSON.parse(this.response);
     l.style.display = "none";
+    lt.style.display = "none";
 
     if (this.status >= 200 && this.status < 400) {
       data.posts.forEach(post => {
@@ -27,14 +30,23 @@ click.onclick = function (){
         h1.setAttribute('class', 'head');
         h1.textContent = post.network;
 
+        if(post.sentiment == 'positive') {
+          h1.style = 'background-image: linear-gradient(120deg, #84fab0 0%, #8fd3f4 100%);';
+        } else if(post.sentiment == 'neutral') {
+          h1.style = 'background-image: linear-gradient(120deg, #ff9a9e 0%, #fecfef 100%);';
+        } else {
+          h1.style = 'background-image: linear-gradient(120deg, #f0df46 0%, #faffaf 100%);';
+        }
+
         const p = document.createElement('p');
         p.setAttribute('class', 'para');
-        p.textContent = "Post : " + post.text + "Sentiment : " + post.sentiment + "Link : " + post.url;
+        p.textContent = 'Post : ' + post.text.substr(0,200) + '...';
 
         const img = document.createElement('img');
         img.setAttribute('class', 'image');
-        var imgsrc = post.user.image;
-        img.setAttribute('src', imgsrc);
+        img.setAttribute('src', post.user.image);
+        img.setAttribute('height','200');
+        img.setAttribute('width','200');
 
         container.appendChild(card);
         card.appendChild(h1);
@@ -45,7 +57,7 @@ click.onclick = function (){
       const errorMessage = document.createElement('p');
       //errorMessage.style.color = red;
       //errorMessage.style.fontSize = 30;
-      errorMessage.textContent = "Error 400 !";
+      errorMessage.textContent = 'Error 400 !';
       app.appendChild(errorMessage);
     }
   }
